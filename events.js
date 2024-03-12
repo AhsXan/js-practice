69d2c7b643158f503e478298bf0dce9f810ff641
Ahsan Ahmed <162038567+AhsXan@users.noreply.github.com>
2024-03-12 11:06:35 +0000

    event start

---
+++ events.js
@@ -0,0 +1,14 @@
+// document.querySelector("#h1").onclick= ()=> {
+
+//     alert("h1 has been selected")
+// }
+// document.querySelector("#h2").onclick= ()=> {
+
+//     alert("h2 has been selected")
+// }
+
+document.querySelector("#h3").addEventListener("click", ()=>{
+
+alert("H3 Has Been Selected")
+
+})
